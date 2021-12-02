21b5af779bf05ca6cc25d18c167d5e207b859ffb
Vini Santos <vini_santos7@live.com>
2021-12-02 19:17:34 -0300

    :tada::art: project trainning DOM and JS, manipulate variables and use loop for! :tada::art:

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 function generatorColors() {
-
   
-
+  const chars = '0123456789ABCDEF'
+  let color = '#'
+     
+  for (let i = 0; i < 6; i++) {
+    color += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return color;
 }
 
-function changeColor() {
+document.getElementById('btn-action').addEventListener('click', () => {
 
-}
+  for(let i = 0; i < 5; i++) {
+    let color = generatorColors()
+    let div_color = document.getElementById(`color-${i+1}`)
+    div_color.style.backgroundColor = color
+    let txt_color = document.getElementById(`txt-color-${i+1}`)
+    txt_color.textContent = (`${color}`)
+  }
+})
